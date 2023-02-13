d4d516e275cd9f38d1822c5c63cb55c54e746bee
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-02-13 14:09:21 +0100

    Update App.java

--- Pilas–Hanoi/src/App.java
+++ Pilas–Hanoi/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+   static int contador = 0;
+   static void moverDisco(int n, String origen, String intermedio, String destino) {
+      if (n == 1) {
+         System.out.println("Mover disco 1 desde " + origen + " hasta " + destino);
+         contador++;
+      } else {
+         moverDisco(n - 1, origen, destino, intermedio);
+         System.out.println("Mover disco " + n + " desde " + origen + " hasta " + destino);
+         contador++;
+         moverDisco(n - 1, intermedio, origen, destino);
+      }
+   }
+   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Introduce el número de discos: ");
+    int n = scan.nextInt();
+    moverDisco(n, "A", "B", "C");
+    System.out.println("\nSe han realizado " + contador + " movimientos");
+ }
 }
