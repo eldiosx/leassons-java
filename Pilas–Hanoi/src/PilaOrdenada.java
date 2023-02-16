1ab2e00318f647fe894a776b75e3e3e587ec5745
pacabruf <pablo.caballos.rufo@gmail.com>
2023-02-16 21:14:22 +0100

    igbyi

--- Pilas–Hanoi/src/PilaOrdenada.java
+++ Pilas–Hanoi/src/PilaOrdenada.java
@@ -3,5 +3,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PilaOrdenada {
-   
+   ArrayList<Integer> lista = new ArrayList<>();
+   void meter(int pieza) {
+      if (pieza>lista.get(lista.size()-1)) {
+         return;
+      } else {
+         lista.add(pieza);
+      }
+      
+   }
+   int sacar() {
+      return lista.remove(lista.size()-1);
+   }
+   int cima() {
+      return lista.get(lista.size()-1);
+   }
 }
